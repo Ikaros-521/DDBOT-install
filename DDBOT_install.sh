3c5684a1450411a77146e19d6a73aa5181898b01
Ikaros <40910637+Ikaros-521@users.noreply.github.com>
2022-11-07 14:18:36 +0800

    适配更多linux架构和指定版本

--- DDBOT_install.sh
+++ DDBOT_install.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+echo "命令参考：bash DDBOT-install.sh <系统架构，不填默认arm64> <程序版本，不填默认1.0.9.2>"
+
+framework="arm64"
+version="1.0.9.2"
+
+if [ "$1" ];then
+    framework=$1
+else
+    echo "系统框架没有传参，按默认配置运行"
+fi
+
+if [ "$2" ];then
+    version=$2
+else
+    echo "程序版本没有传参，按默认配置运行"
+fi
+
+echo "系统架构=$framework 版本=$version"
+
 # 下载官方bot压缩包 版本为v1.0.9.2
-curl https://gh.ddlc.top/https://github.com/Sora233/DDBOT/releases/download/v1.0.9.2/DDBOT-v1.0.9.2-linux-arm64.tar.gz --output DDBOT-v1.0.9.2-linux-arm64.tar.gz
+curl https://gh.ddlc.top/https://github.com/Sora233/DDBOT/releases/download/v$version/DDBOT-v$version-linux-$framework.tar.gz --output DDBOT-v$version-linux-$framework.tar.gz
 # 创建文件夹
 mkdir DDBOT
 # 移动压缩包到指定文件夹
-mv DDBOT-v1.0.9.2-linux-arm64.tar.gz DDBOT
+mv DDBOT-v$version-linux-$framework.tar.gz DDBOT
 # 进入文件夹
 cd DDBOT
 # 解压
-tar -zxvf DDBOT-v1.0.9.2-linux-arm64.tar.gz
+tar -zxvf DDBOT-v$version-linux-$framework.tar.gz
 # 添加执行权限
 chmod +x DDBOT
 # 前台运行
